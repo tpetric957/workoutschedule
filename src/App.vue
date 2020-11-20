f0eb87095f9164e2b9f287dfee844f433f3fc617
tpetric957 <72700129+tpetric957@users.noreply.github.com>
2020-11-20 17:37:02 +0100

    Creation
    Start of the project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <b-navbar class="type is-danger fixed-top">
+      <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <i class="fas fa-dumbbell"></i>
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                About
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Contact
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-dark">
+                        Sign up
+                    </a>
+                    <a class="button is-dark">
+                        Login
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +43,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <b-image
+            src="../assets/image.jpg"
+            alt="A random image"
+            ratio="6by4"
+            :rounded="rounded"
+        ></b-image>
+  </div>
+</template>
+
+<script>
+
+</script>
